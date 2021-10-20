d1cc55999caeb3642f9bf0f39e289d70dcc061ca
Hadi Saleh <89351369+TheLedgerOfJoudi@users.noreply.github.com>
2021-10-20 21:42:00 +0300

    Update WalletFactory.sol

--- WalletFactory.sol
+++ WalletFactory.sol
@@ -5,9 +5,10 @@ import "./MultiSig.sol";
 
 contract WalletFactory {
     address public wallet = 0x0000000000000000000000000000000000000000;
-    
+    mapping public userToWallet (address => address);
     constructor (address _owner1, address _owner2){
     wallet = address(new MultiSig(_owner1, _owner2));
+    userToWallet[msg.sender] = wallet;
     }
-    
+    //EIP1167
 }
